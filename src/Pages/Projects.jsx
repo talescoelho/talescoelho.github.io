c5231cc15d9e44920f1358bf313c779d7db55d88
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-16 20:01:06 -0300

    carroussel

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,29 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import todoList from './projects/TodoList/todoList.png';
 import PixelArt from './projects/PixelArt/PixelArt.png';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Project.css';
 
 function Projects() {
   return (
     <main>
-      <div className="container-projects">
-        <Link to="/projects/todolist" className="link-project">
-          <h1 className="title-project">
-            Todo List
-          </h1>
-          <div className="card-project">
-            <img src={ todoList } alt="todo List" />
-          </div>
-        </Link>
-        <Link to="/projects/pixelart" className="link-project">
-          <h1 className="title-project">
-            Pixel Art
-          </h1>
-          <div className="card-project">
-            <img src={ PixelArt } alt="todo List" />
-          </div>
-        </Link>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img src={ todoList } alt="todo List" className="d-block w-100" />
+          <Carousel.Caption>
+            <Link to="/projects/todolist">
+              <h1 className="title-project">
+                Todo List
+              </h1>
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={ PixelArt } alt="todo List" className="d-block w-100" />
+          <Carousel.Caption>
+            <Link to="/projects/pixelart">
+              <h1 className="title-project">
+                Pixel Art
+              </h1>
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </main>
   )
 }
